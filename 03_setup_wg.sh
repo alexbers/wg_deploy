e4b384c195df1c3b107154eaf5165a1dda019c6f
Alexander Bersenev <bay@hackerdom.ru>
2022-03-04 19:58:56 +0500

    detect docker

--- 03_setup_wg.sh
+++ 03_setup_wg.sh
@@ -10,3 +10,9 @@ systemctl start wg-quick@bay_srv
 if [ $? -eq 0 ]; then
   echo "Wireguard should be running now, configs are in ./configs"
 fi
+
+if pidof dockerd -q; then
+  echo "Warning: it seems you have Docker installed. It sets firewall rules"
+  echo "which can prevent VPN from working. Usual command to fix:"
+  echo "iptables -P FORWARD ACCEPT"
+fi
