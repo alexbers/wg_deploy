5c766a7b58ccc88a486c26ab059b68a6908f27c6
Alexander Bersenev <bay@hackerdom.ru>
2026-02-17 16:39:02 +0500

    typo

--- 03_setup_wg.sh
+++ 03_setup_wg.sh
@@ -11,7 +11,7 @@ if [ $? -eq 0 ]; then
   echo "Wireguard should be running now, configs are in ./configs"
 fi
 
-if -q pidof dockerd; then
+if pidof -q dockerd; then
   echo "Warning: it seems you have Docker installed. It sets firewall rules"
   echo "which can prevent VPN from working. Usual command to fix:"
   echo "iptables -P FORWARD ACCEPT"
